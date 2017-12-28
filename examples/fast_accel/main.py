2a67165947016dc977c1ee6a8fb2ed74fc38e554
Dave Astels <dastels@daveastels.com>
2017-12-28 16:33:03 -0500

    Make the API more in line with others

--- examples/fast_accel/main.py
+++ examples/fast_accel/main.py
@@ -8,6 +8,6 @@ i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_lsm303.LSM303(i2c)
 
 while True:
-	accel_x, accel_y, accel_z = sensor.accel
+	accel_x, accel_y, accel_z = sensor.accelerometer
 
 	print('{0:10.3f} {1:10.3f} {2:10.3f}'.format(accel_x, accel_y, accel_z))
--- examples/slow_both/main.py
+++ examples/slow_both/main.py
@@ -8,8 +8,8 @@ i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_lsm303.LSM303(i2c)
 
 while True:
-	accel_x, accel_y, accel_z = sensor.accel
-	mag_x, mag_y, mag_z = sensor.mag
+	accel_x, accel_y, accel_z = sensor.accelerometer
+	mag_x, mag_y, mag_z = sensor.magnetometer
 
 	print('Acceleration (m/s^2): ({0:10.3f}, {1:10.3f}, {2:10.3f})'.format(accel_x, accel_y, accel_z))
 	print('Magnetometer (gauss): ({0:10.3f}, {1:10.3f}, {2:10.3f})'.format(mag_x, mag_y, mag_z))
