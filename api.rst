d464d0cd4206d30ba12223750504228cd14607d2
Dave Astels <dastels@daveastels.com>
2018-01-20 21:18:11 -0500

    Rename library

--- api.rst
+++ api.rst
@@ -1,5 +1,5 @@
 
 .. If you created a package, create one automodule per module in the package.
 
-.. automodule:: adafruit_lsm303
+.. automodule:: lsm303
    :members:
--- lsm303.py
+++ lsm303.py
@@ -20,7 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 """
-`adafruit_lsm303`
+`lsm303`
 ====================================================
 
 
