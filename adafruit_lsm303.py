0d8f825677a7a82568c0714b71fc9026235ad6db
Kattni Rembor <kattni@adafruit.com>
2018-08-05 15:10:41 -0400

    PyPi setup.

--- adafruit_lsm303.py
+++ adafruit_lsm303.py
@@ -136,6 +136,8 @@ _GAUSS_TO_MICROTESLA       = 100.0        # Gauss to micro-Tesla multiplier
 # pylint: enable=bad-whitespace
 
 
+# pylint: disable=useless-object-inheritance
+# Pylint issue needs to be fixed.
 class LSM303(object):
     """Driver for the LSM303 accelerometer/magnetometer."""
 
